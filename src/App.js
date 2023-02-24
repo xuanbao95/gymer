434fcb167270fbda6076aef3fc9ddb63cf1b709f
Thuan0907 <jinkin0019@gmail.com>
2023-02-24 21:21:28 +0700

    sliderPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import SliderPage from "./pages/SliderPage/SliderPage";
 
 function App() {
   return (
     <div>
-      
+      <SliderPage />
     </div>
   );
 }
