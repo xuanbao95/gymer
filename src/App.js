c6de7dac032255b13b892c2d57f08068046cd0cb
lota <tidihoc123@gmail.com>
2023-02-24 21:12:32 +0700

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderPage from './Pages/HeaderPages/HeaderPage';
 
 function App() {
   return (
     <div>
-      
+      <HeaderPage />
     </div>
   );
 }
