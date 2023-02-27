6a299564c02895c1e51c9a1b0851f3c9f19e94bc
Thuan0907 <jinkin0019@gmail.com>
2023-02-27 19:37:11 +0700

    feature/aboutUs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import HeaderPage from './Pages/HeaderPages/HeaderPage';
+import logo from "./logo.svg";
+import "./App.css";
+import HeaderPage from "./pages/HeaderPages/HeaderPage";
+import AboutUs from "./pages/AboutUs/AboutUs";
 
 function App() {
   return (
     <div>
-      <HeaderPage />
+      {/* <HeaderPage /> */}
+      <AboutUs />
     </div>
   );
 }
